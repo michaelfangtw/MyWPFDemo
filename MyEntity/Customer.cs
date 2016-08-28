ce86dc6ba51f039de1b938165563509cc1468305
Michael Fang <yingchih.fang@gmail.com>
2016-08-28 19:35:25 +0800

    WPF Demo first commit

---
+++ MyEntity/Customer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyEntity
+{
+    [Serializable]  //必須要可序列化
+    public class Customer
+    {
+        public string CustomerID { get; set; }
+        public string CustomerName { get; set; }
+        public string Email { get; set; }
+        public DateTime Birthday { get; set; }
+
+        public DateTime CreateTime { get; set; }
+    }
+
+}
