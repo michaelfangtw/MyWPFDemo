35aa19f6c083d14945ec189f21c0b0fa56b5afee
michaelfang <fij@pchome.com.tw>
2016-09-08 16:57:08 +0800

    customer.cs updated

--- src/MyEntity/Customer.cs
+++ src/MyEntity/Customer.cs
@@ -13,7 +13,6 @@ namespace MyEntity
         public string CustomerName { get; set; }
         public string Email { get; set; }
         public DateTime Birthday { get; set; }
-
         public DateTime CreateTime { get; set; }
     }
 
